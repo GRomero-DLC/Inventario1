5f85dacc5d202fad2f04a29b834cc5b564ffea85
GRomero <GRomero@DESKTOP-5KIGQ3P>
2023-02-07 00:31:39 -0500

    Agregar archivos de proyecto.

---
+++ Inventario.COMMON/Entidades/Entrega.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventario.COMMON.Entidades
+{
+    public class Entrega:Base
+    {
+        public DateTime FechaHoraSolicitud { get; set; }
+        public DateTime FechaEntrega { get; set; }
+        public DateTime? FechaEntregaReal { get; set; }
+        public List<Material>
+            MaterialesPrestados      { get; set; }
+        public Trabajador Solicitante { get; set; }
+        public Trabajador EncargadoDeAlmacen { get; set; }
+                    
+    }
+}
---
+++ Inventario.DAL/RepositorioDeTrabajadores.cs
@@ -0,0 +1,31 @@
+﻿using Inventario.COMMON.Entidades;
+using Inventario.COMMON.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventario.DAL
+{
+    public class RepositorioDeTrabajadores : IRepositorio<Trabajador>
+    {
+        //Base de datos NoSQL: Orientada a Documentos -> LiteDB
+        public List<Trabajador> Read => throw new NotImplementedException();
+
+        public bool Create(Trabajador entidad)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(Trabajador entidad)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(Trabajador entidadOriginal, Trabajador entidadModificada)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
